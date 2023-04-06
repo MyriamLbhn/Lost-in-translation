02acda3807967db65af98217c497ed577167d1e5
MyriamLbhn <lebihan.myriam@gmail.com>
2023-04-06 14:31:42 +0200

    add create_database.py

---
+++ create_database.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+connexion = sqlite3.connect("bdd.db")
+curseur = connexion.cursor()
+
+curseur.execute("""
+                CREATE TABLE IF NOT EXISTS ObjetsTrouves (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    type TEXT NOT NULL,
+                    nom_gare TEXT NOT NULL,
+                    date DATETIME NOT NULL,
+                    FOREIGN KEY(date) REFERENCES Temperatures(date)
+                    FOREIGN KEY(nom_gare) REFERENCES Gares(nom_gare)
+                )
+                """)
+connexion.commit()
+
+
+curseur.execute("""
+                CREATE TABLE IF NOT EXISTS Temperatures (
+                    date DATETIME PRIMARY KEY,
+                    temperature INTEGER NOT NULL
+                )
+                """)
+connexion.commit()
+
+curseur.execute("""
+                CREATE TABLE IF NOT EXISTS Gares (
+                    nom_gare TEXT PRIMARY KEY,
+                    frequentation_2019 INTEGER NOT NULL,
+                    frequentation_2020 INTEGER NOT NULL,
+                    frequentation_2021 INTEGER NOT NULL,
+                    frequentation_2022 INTEGER NOT NULL,
+                    latitude FLOAT NOT NULL,
+                    longitude FLOAT NOT NULL
+                )
+                """)
+connexion.commit()
+
+connexion.close()
