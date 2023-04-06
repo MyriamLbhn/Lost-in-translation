502b60ab8fde796229e59ec1fe312ebbd6e855d4
Thomas59 <thomas.lemay59@outlook.fr>
2023-04-06 15:41:45 +0200

    ajout script pour mettre les informations des objets trouver dans la bdd

--- API_SNCF.py
+++ API_SNCF.py
@@ -1,25 +1,36 @@
 import requests
+import sqlite3
 import pandas as pd
+import re
+from datetime import datetime
 
 gares_parisiennes = ["Paris Gare de Lyon", "Paris Montparnasse", "Paris Gare du Nord", "Paris Est" , "Paris Saint-Lazare" , "Paris Austerlitz" , 'Paris Bercy']
 annees = ["2019", "2020" , "2021" , "2022"]
 
-# Création d'un DataFrame vide
-df = pd.DataFrame()
+connexion = sqlite3.connect("bdd.db")
+curseur = connexion.cursor()
+
 i = 0
 for gare in gares_parisiennes:
     for annee in annees:
         print(i)
         response = requests.get(f"https://ressources.data.sncf.com/api/records/1.0/search/?dataset=objets-trouves-restitution&q=&rows=-1&sort=-date&facet=date&facet=gc_obo_date_heure_restitution_c&facet=gc_obo_gare_origine_r_name&facet=gc_obo_nature_c&facet=gc_obo_type_c&facet=gc_obo_nom_recordtype_sc_c&refine.gc_obo_gare_origine_r_name={gare}&refine.date={annee}")
         
-        # Récupération des enregistrements
         records = response.json()["records"]
 
-        # Conversion des enregistrements en DataFrame
-        records_df = pd.DataFrame([record["fields"] for record in records])
+        for record in records:
+            fields = record["fields"]
+            date_string = fields["date"]
+            date_string = re.sub(r'\+\d\d:\d\d$', '', date_string)  # Supprime le décalage horaire
+            date = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S").strftime('%Y-%m-%d %H:%M:%S')
+            type_objet = fields["gc_obo_nature_c"]
+            nom_gare = fields["gc_obo_gare_origine_r_name"]
 
-        # Ajout des enregistrements au DataFrame global
-        df = pd.concat([df, records_df], ignore_index=True)
+            curseur.execute("""
+                            INSERT INTO ObjetsTrouves (type, nom_gare, date)
+                            VALUES (?, ?, ?)
+                            """, (type_objet, nom_gare, date))
         i += 1
-# Enregistrement du DataFrame dans un fichier CSV
-df.to_csv("objets_trouves.csv", index=False)
+
+connexion.commit()
+connexion.close()
