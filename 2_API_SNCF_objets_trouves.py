77c2a4ffb9dd5d849aab652ec01bbdfec20341f3
MyriamLbhn <lebihan.myriam@gmail.com>
2023-04-12 15:50:58 +0200

    update objet trouve fonctionel

--- 2_API_SNCF_objets_trouves.py
+++ 2_API_SNCF_objets_trouves.py
@@ -1,19 +1,23 @@
-import json
-import os
 import requests
 import sqlite3
 import re
-from datetime import datetime, date
+from datetime import datetime
 import logging
 
 import pandas as pd
 
 logging.basicConfig(level=logging.INFO)
 
-def ajouter_objets_trouves_a_bdd():
-    gares_parisiennes = ["Paris Gare de Lyon", "Paris Montparnasse", "Paris Gare du Nord", "Paris Est" , "Paris Saint-Lazare" , "Paris Austerlitz" , 'Paris Bercy']
-    annees = ["2019", "2020" , "2021" , "2022"]
+annees = []
+current_year = datetime.now().year
 
+for year in range(2019, current_year + 1):
+    annees.append(str(year))
+    
+gares_parisiennes = ["Paris Gare de Lyon", "Paris Montparnasse", "Paris Gare du Nord", "Paris Est" , "Paris Saint-Lazare" , "Paris Austerlitz" , 'Paris Bercy']
+
+    
+def ajouter_objets_trouves():
     connexion = sqlite3.connect("bdd.db")
     curseur = connexion.cursor()
 
@@ -43,54 +47,53 @@ def ajouter_objets_trouves_a_bdd():
     connexion.close()
 
 
-def update_objets_trouves_a_bdd():
-    gares_parisiennes = ["Paris Gare de Lyon", "Paris Montparnasse", "Paris Gare du Nord", "Paris Est" , "Paris Saint-Lazare" , "Paris Austerlitz" , 'Paris Bercy']
-
-    # Connexion à la base de données et récupération de la dernière date enregistrée
+def update_objets_trouves_current_year():
+    # Connexion à la base de données et récupération de l'année en cours
     connexion = sqlite3.connect("bdd.db")
     curseur = connexion.cursor()
-    curseur.execute("SELECT MAX(date) FROM ObjetsTrouves")
-    last_date_string = curseur.fetchone()[0]
-    last_date = datetime.strptime(last_date_string, '%Y-%m-%d %H:%M:%S') if last_date_string else datetime(2019, 1, 1 , 0, 0, 0)
-    connexion.close()
-
-    # Récupération des données depuis la dernière date jusqu'à aujourd'hui
-    date_today = datetime.today()
-    date_start = last_date.date()
-    date_end = date_today.date()
+    current_year = datetime.now().year
     
-
-    # Insertion des données dans la base de données
-    connexion = sqlite3.connect("bdd.db")
-    curseur = connexion.cursor()
-
-
-
+    # Effacer toutes les données de l'année en cours
+    curseur.execute(f"DELETE FROM ObjetsTrouves WHERE annee = {current_year}")
+    connexion.commit()
+    
+    # Récupération des données pour l'année en cours
+    logging.info(f"Récupération des données pour l'année en cours")
     for gare in gares_parisiennes:
-        logging.info(f"Récupération des données pour la gare {gare} depuis {date_start} jusqu'à {date_end}")
-        response = requests.get(f"https://ressources.data.sncf.com/api/records/1.0/search/?dataset=objets-trouves-restitution&q=&rows=-1&sort=-date&facet=date&facet.range=date&facet=gc_obo_date_heure_restitution_c&facet=gc_obo_gare_origine_r_name&facet=gc_obo_nature_c&facet=gc_obo_type_c&facet=gc_obo_nom_recordtype_sc_c&refine.gc_obo_gare_origine_r_name={gare}")
-
+        response = requests.get(f"https://ressources.data.sncf.com/api/records/1.0/search/?dataset=objets-trouves-restitution&q=&rows=-1&sort=-date&facet=date&facet=gc_obo_date_heure_restitution_c&facet=gc_obo_gare_origine_r_name&facet=gc_obo_nature_c&facet=gc_obo_type_c&facet=gc_obo_nom_recordtype_sc_c&refine.gc_obo_gare_origine_r_name={gare}&refine.date={current_year}")
+        
         records = response.json()["records"]
         for record in records:
-            # Ajouter le record à la liste all_records
             fields = record["fields"]
             date_string = fields["date"]
             date_string = re.sub(r'\+\d\d:\d\d$', '', date_string)  # Supprime le décalage horaire
-            date = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S").date()
-
+            date = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S").strftime('%Y-%m-%d %H:%M:%S')
             type_objet = fields["gc_obo_nature_c"]
             nom_gare = fields["gc_obo_gare_origine_r_name"]
-            annee = pd.to_datetime(date).year
             semaine = pd.to_datetime(date).isocalendar().week
             
-            if annee >= 2019 :
-                curseur.execute("""
+            curseur.execute("""
                             INSERT INTO ObjetsTrouves (type, nom_gare, date, annee, semaine)
                             VALUES (?, ?, ?, ?, ?)
-                             """, (type_objet, nom_gare, date, annee, semaine))
+                            """, (type_objet, nom_gare, date, current_year, semaine))
 
     connexion.commit()
     connexion.close()
 
-# ajouter_objets_trouves_a_bdd()
-update_objets_trouves_a_bdd()
+
+# def delete_entry(date):
+#     connexion = sqlite3.connect("bdd.db")
+#     cursor = connexion.cursor()
+
+#     # convertir la date en format datetime
+#     date_obj = datetime.strptime(date, '%Y-%m-%d').date()
+
+#     # supprimer les entrées à partir de la date indiquée jusqu'à aujourd'hui
+#     cursor.execute("""
+#                     DELETE FROM ObjetsTrouves
+#                         WHERE date >= ?
+#                     """, (date_obj,))
+
+#     connexion.commit()
+#     connexion.close()
+
